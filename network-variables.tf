282e6aef76a2bae0d52d235b3c238fadd62253db
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2022-02-10 12:10:18 +0100

    Initial commit

--- network-variables.tf
+++ network-variables.tf
@@ -4,18 +4,21 @@
 
 # AWS AZ
 variable "aws_az" {
+  type        = string
   description = "AWS AZ"
-  default = "eu-west-1c"
+  default     = "eu-west-1c"
 }
 
 # VPC Variables
 variable "vpc_cidr" {
+  type        = string
   description = "CIDR for the VPC"
-  default = "10.1.64.0/18"
+  default     = "10.1.64.0/18"
 }
 
 # Subnet Variables
 variable "vpc_public_subnet_cidr" {
+  type        = string
   description = "CIDR for the public subnet"
-  default = "10.1.64.0/24"
+  default     = "10.1.64.0/24"
 }
