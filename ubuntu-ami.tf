9d5032953098fa4774f48c188134af03b3a1de61
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2022-02-10 12:24:01 +0100

    Initial commit

--- ubuntu-ami.tf
+++ ubuntu-ami.tf
@@ -2,7 +2,7 @@
 # Get latest Ubuntu Linux AMI with Terraform #
 ##############################################
 
-# Get latest Ubuntu Linux Trusty 14.04 AMI
+# Get latest Ubuntu Linux Trusty Tahr 14.04 AMI
 data "aws_ami" "ubuntu-linux-1404" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu-linux-1404" {
   }
 }
 
-# Get latest Ubuntu Linux Xenial 16.04 AMI
+# Get latest Ubuntu Linux Xenial Xerus 16.04 AMI
 data "aws_ami" "ubuntu-linux-1604" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
@@ -30,7 +30,7 @@ data "aws_ami" "ubuntu-linux-1604" {
   }
 }
 
-# Get latest Ubuntu Linux Bionic 18.04 AMI
+# Get latest Ubuntu Linux Bionic Beaver 18.04 AMI
 data "aws_ami" "ubuntu-linux-1804" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
@@ -44,21 +44,7 @@ data "aws_ami" "ubuntu-linux-1804" {
   }
 }
 
-# Get latest Ubuntu Linux Disco 19.04 AMI
-data "aws_ami" "ubuntu-linux-1904" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-# Get latest Ubuntu Linux Disco 20.04 AMI
+# Get latest Ubuntu Linux Focal Fossa 20.04 AMI
 data "aws_ami" "ubuntu-linux-2004" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
@@ -84,10 +70,6 @@ data "aws_ami" "ubuntu-linux-2004" {
 
 # output "ubuntu_1804_ami_id" {
 #   value = data.aws_ami.ubuntu-linux-1804.id
-# }
-
-# output "ubuntu_1904_ami_id" {
-#   value = data.aws_ami.ubuntu-linux-1904.id
 # }
 
 # output "ubuntu_2004_ami_id" {
