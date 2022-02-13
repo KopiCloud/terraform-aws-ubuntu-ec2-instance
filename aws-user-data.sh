007ffdf2d2947039bdbdfa8f231c0298f30b5418
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2022-02-13 09:44:17 +0100

    Initial commit

--- aws-user-data.sh
+++ aws-user-data.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-echo "<h1>Test AWS</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>AWS Virtual Machine deployed with Terraform</h1>" | sudo tee /var/www/html/index.html
